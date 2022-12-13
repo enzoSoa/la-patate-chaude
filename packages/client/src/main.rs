c8ef69b2debbb22e3df368679c1935ebd42fd16f
Enzo Soares <enzoayrsoares@gmail.com>
2022-12-13 15:13:25 +0100

    Adding client connection to server

--- packages/client/src/main.rs
+++ packages/client/src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use std::net::TcpStream;
+
+fn main() -> std::io::Result<()> {
+    if let Ok(stream) = TcpStream::connect("127.0.0.1:80") {
+        println!("{:#?}", stream);
+    } else { println!("Connection failed") };
+
+    Ok(())
 }
