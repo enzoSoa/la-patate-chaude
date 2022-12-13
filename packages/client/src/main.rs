89306d31ae4ec181e60739d0baa427f629bb9eea
Enzo Soares <enzoayrsoares@gmail.com>
2022-12-13 16:20:05 +0100

    Adding args support on client

--- packages/client/src/main.rs
+++ packages/client/src/main.rs
@@ -1,9 +1,21 @@
+use std::env;
 use std::net::TcpStream;
 
 fn main() -> std::io::Result<()> {
-    if let Ok(stream) = TcpStream::connect("127.0.0.1:80") {
-        println!("{:#?}", stream);
+    let server_address = retrieve_server_address();
+    if let Ok(_stream) = TcpStream::connect(server_address) {
+        println!("Successfully connected");
     } else { println!("Connection failed") };
-
     Ok(())
+}
+
+fn retrieve_server_address() -> String {
+    match retrieve_args().get(1) {
+        Some(address) => String::from(address),
+        None => String::from("127.0.0.1:80")
+    }
+}
+
+fn retrieve_args() -> Vec<String> {
+    return env::args().collect();
 }
